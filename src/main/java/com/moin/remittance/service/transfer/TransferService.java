13a91bb77afa849e7fe818398d55076df9845e22
황광회 <jack7141@nate.com>
2024-10-29 17:18:56 +0900

    ADD: 송금 로직 init

--- src/main/java/com/moin/remittance/service/transfer/TransferService.java
+++ src/main/java/com/moin/remittance/service/transfer/TransferService.java
@@ -7,5 +7,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface TransferService {
+    /**
+     *
+     * @param dto
+     * 수수료 = 보내는 금액 * 수수료율 + 고정수수료
+     *  3050 = 10000 * 0.005 + 3000
+     *  받는 금액 = 10000 - 3050
+     */
     public QuoteRespDTO calculateQuote(QuoteReqDTO dto);
 }
