d1abea15dd4039fe5b674c88a2532b62b3fb78de
황광회 <jack7141@nate.com>
2024-10-31 19:13:53 +0900

    Fix: Trade Implements Layer 분리

--- src/main/java/com/moin/remittance/service/trade/Impl/TradeServiceImpl.java
+++ src/main/java/com/moin/remittance/service/trade/Impl/TradeServiceImpl.java
@@ -1,4 +1,81 @@
 package com.moin.remittance.service.trade.Impl;
 
-public class TradeServiceImpl {
+import com.moin.remittance.domain.dto.trade.TradeDTO;
+import com.moin.remittance.domain.dto.trade.TradeHistoryDTO;
+import com.moin.remittance.domain.dto.trade.TradeRespDTO;
+import com.moin.remittance.domain.entity.Trade.TradeEntity;
+import com.moin.remittance.domain.entity.member.MemberEntity;
+import com.moin.remittance.exception.MemberNotFoundException;
+import com.moin.remittance.exception.QuoteExpiredException;
+import com.moin.remittance.repository.MemberRepository;
+import com.moin.remittance.repository.TradeRepository;
+import com.moin.remittance.service.trade.TradeService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TradeServiceImpl implements TradeService {
+
+    private final TradeRepository tradeRepository;
+    private final MemberRepository memberRepository;
+
+
+    public TradeEntity saveTrade(TradeDTO dto, MemberEntity currentUser) {
+        return tradeRepository.save(dto.toEntity(dto, currentUser));
+    }
+
+    public TradeEntity acceptQuoteTransaction(Long id) {
+        TradeEntity tradeEntity = tradeRepository.findByTranscationId(id);
+        if (tradeEntity == null) {
+            throw new QuoteExpiredException("찾을 수 없는 송금장입니다.");
+        }
+
+        OffsetDateTime currentTime = OffsetDateTime.now();
+        Duration duration = Duration.between(tradeEntity.getRequestedDate(), currentTime);
+        if (duration.toMinutes() > 10) {
+            throw new QuoteExpiredException("송금 접수 시간이 만료되었습니다.");
+        }
+        return tradeEntity;
+    }
+
+    public TradeRespDTO getUserTrade(String userId) {
+        MemberEntity memberEntity = memberRepository.findByUserId(userId);
+        if (memberEntity == null) {
+            throw new MemberNotFoundException("존재하지 않는 회원입니다.");
+        }
+        List<TradeHistoryDTO> transferHistory = memberEntity.getTrades().stream().map(trade ->
+                TradeHistoryDTO.builder()
+                        .sourceAmount(trade.getSourceAmount())
+                        .fee(trade.getFee())
+                        .usdExchangeRate(trade.getUsdExchangeRate())
+                        .usdAmount(trade.getUsdAmount())
+                        .targetCurrency(trade.getTargetCurrency())
+                        .exchangeRate(trade.getExchangeRate())
+                        .targetAmount(trade.getTargetAmount())
+                        .requestedDate(trade.getRequestedDate().toLocalDateTime())
+                        .build()
+        ).collect(Collectors.toList());
+
+        return TradeRespDTO.builder()
+                .userId(memberEntity.getUserId())
+                .name(memberEntity.getName())
+                .todayTransferCount((int) transferHistory.stream()
+                        .filter(trade -> trade.getRequestedDate().toLocalDate().isEqual(LocalDate.now()))
+                        .count()) // 오늘 송금 횟수
+                .todayTransferUsdAmount(transferHistory.stream()
+                        .filter(trade -> trade.getRequestedDate().toLocalDate().isEqual(LocalDate.now()))
+                        .mapToDouble(TradeHistoryDTO::getUsdAmount)
+                        .sum()) // 오늘 송금 금액
+                .history(transferHistory)
+                .build();
+    }
 }
